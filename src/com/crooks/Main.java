820ffd84bba2948303dd8610cbef7a3df3185ba2
jonathandavidblack <jonnybl915@gmail.com>
2016-06-16 13:32:54 -0400

    added basic create table method

--- src/com/crooks/Main.java
+++ src/com/crooks/Main.java
@@ -6,8 +6,14 @@ import spark.Spark;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
+
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, username VARCHAR, password VARCHAR)");
+    }
 
     public static void main(String[] args) throws SQLException {
         Server.createWebServer().start();
