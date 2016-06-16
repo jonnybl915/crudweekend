4078fc4511cc323dcb43e0f057334ab9044ec6ee
John Crooks <johnmcrooks@gmail.com>
2016-06-16 12:52:05 -0400

    Initial Commit - libraries added, 2 default classes created, default html file created

---
+++ src/com/crooks/Item.java
@@ -0,0 +1,7 @@
+package com.crooks;
+
+/**
+ * Created by johncrooks on 6/16/16.
+ */
+public class Item {  //This item needs to be changed to reflect whatever we end up tracking
+}
---
+++ src/com/crooks/User.java
@@ -0,0 +1,30 @@
+package com.crooks;
+
+/**
+ * Created by johncrooks on 6/16/16.
+ */
+public class User {
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
