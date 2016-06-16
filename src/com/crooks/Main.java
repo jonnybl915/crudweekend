ad345da2e95f27238ad4f7a0bc2dd1577467b564
jonathandavidblack <jonnybl915@gmail.com>
2016-06-16 13:26:12 -0400

    initialized spark

--- src/com/crooks/Main.java
+++ src/com/crooks/Main.java
@@ -1,8 +1,19 @@
 package com.crooks;
 
+import org.h2.tools.Server;
+import spark.Spark;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws SQLException {
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+
+        Spark.staticFileLocation("public");
+        Spark.init();
     }
 }
