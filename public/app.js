7580716d33e455f4d27f21e2bb019c1ca28057c4
Robert Suber <robertsuber@Roberts-MacBook-Pro.local>
2016-06-19 12:10:33 -0400

    New Javascript

--- public/app.js
+++ public/app.js
@@ -1,11 +1,22 @@
+/* Initialize map at the beginning of the page */
+function initMap() {
+  var mapDiv = document.getElementById('map');
+  var map = new google.maps.Map(mapDiv, {
+      center: {lat: 44.540, lng: -78.546},
+      zoom: 8
+  });
+}
+
 $(document).ready(function(){
-  logInPage.events();
+  skipToMyLou.events();
 })
 
-var logInPage = {
+var skipToMyLou = {
 
   events: function() {
   $(".mainPage").toggle();
+
+  /* USER NAME AND PASSWORD */
   $('.signIn').on("click", function(event){
     event.preventDefault();
     $.ajax({
@@ -16,24 +27,24 @@ var logInPage = {
           username:$("#Username").val(),
           password:$('#Password').val(),
         }),
-      success: function(data) { if(1){
+      success: function(data) { if(username && password !== null) {
       console.log("This worked", data);
-      $('.logInPage').toggle();
-      $(".mainPage").toggle();
+      $('.logInPage').fadeToggle(3000);
+      $(".mainPage").fadeToggle(3000);
     }
-      },
-      error: function(err) { if(-1){
+  },
+      error: function(err) {
         console.error("OH CRAP", err);
       alert("HOLD IT!");
-    }
       }
     })
   });
+
 },
 Read: function() {
   $.ajax({
     method:"GET",
-    url:"/login",
+    url:"login",
   success:function(data) {
     console.log(data);
     data = JSON.parse(data)
@@ -41,9 +52,4 @@ Read: function() {
   error:function(err) {
     console.err("Oh SHit!!!",data)
   }
-  })
-}
-}
-var mainPage = {
-
-}
+})}}
